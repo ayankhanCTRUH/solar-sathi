b63583150927f980f38b2bb3a2465278c93d01e0
Gulzeesh <gulzeesh@ctruh.com>
2025-07-24 16:34:53 +0530

    update: ReactQuery type

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,9 +1,11 @@
 'use client';
+import { ReactQueryProviderProps } from '@/types';
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 import { QueryClient } from '@tanstack/react-query';
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { FC } from 'react';
 
-const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+const ReactQueryProvider: FC<ReactQueryProviderProps> = ({ children }) => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
