c5ff962b7ad9f5bfb04d0204cd2e4bc4f1c661c9
niharika-ctruh <niharika@ctruh.com>
2025-07-25 15:45:59 +0530

    revert: middle page one added

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,3 @@
-import { ReactNode } from 'react';
-
 export interface ReactQueryProviderProps {
-  children: ReactNode;
-}
-
-interface TextItem {
-  text: string;
-  variant?: 'blue';
-  color?: string;
-  break?: boolean;
-}
-
-export interface MixColorsTextProps {
-  content: TextItem[];
-  className?: string;
-}
-
-export type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'disable';
-
-export interface ButtonProps {
-  content: string;
-  variant?: ButtonVariant;
-  className?: string;
-  leftIcon?: ReactNode;
-  rightIcon?: ReactNode;
-  onClick?: () => void;
-  isLoading?: boolean;
+  children: React.ReactNode;
 }
