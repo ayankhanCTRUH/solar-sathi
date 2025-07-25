16e829ff87119796ab98c7e164965bb9cc9dcda2
Yaseer <yaseer@ctruh.com>
2025-07-25 19:16:42 +0530

    `Refactor BreadCrumbs component: update conditionals and aria-label`

--- src/components/ui/BreadCrumbs/index.tsx
+++ src/components/ui/BreadCrumbs/index.tsx
@@ -12,34 +12,33 @@ interface BreadCrumbsProps {
 }
 
 const BreadCrumbs = ({ items }: BreadCrumbsProps) => {
-  if (!items || items.length === 0) return null;
+  if (!items?.length) return null;
 
   return (
-    <div className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex items-center gap-0.5 rounded-xl border-[1.5px] p-4 text-white">
+    <div
+      className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex w-full items-center gap-0.5 rounded-xl border-[1.5px] p-4 text-white"
+      aria-label="Breadcrumb"
+    >
       {items.map((item, index) => {
-        const isLast = index === items.length - 1;
         const isFirst = index === 0;
-        const showLabel = items.length <= 2 || !isFirst;
-        const shouldTruncate = items.length > 2;
+        const isLast = index === items.length - 1;
+        const shouldShowLabel = items.length <= 2 || !isFirst;
+        const shouldTruncate = items.length > 2 && !isFirst && !isLast;
 
         return (
           <Fragment key={index}>
             <Link
               href={item.href}
               className={`flex items-center gap-1 text-2xl/8.5 select-none ${
-                !isFirst && isLast
+                isLast && !isFirst
                   ? 'text-secondary-500 pointer-events-none font-semibold'
                   : 'font-normal'
               }`}
             >
               {isFirst && <HomeIcon className="m-2 shrink-0" />}
-              {showLabel && (
+              {shouldShowLabel && (
                 <span
-                  className={
-                    shouldTruncate && !isLast && !isFirst
-                      ? 'max-w-20 truncate'
-                      : ''
-                  }
+                  className={`${shouldTruncate ? 'max-w-20 truncate' : ''}`}
                 >
                   {item.label}
                 </span>
