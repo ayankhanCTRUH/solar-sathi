335187ce63c66b68a1d7e2305e5a0cef310d10f8
Yaseer <yaseer@ctruh.com>
2025-07-25 17:02:59 +0530

    `Update BreadCrumbs component to include a nested link and remove a commented out section`

--- src/components/ui/BreadCrumbs/index.tsx
+++ src/components/ui/BreadCrumbs/index.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 const BreadCrumbs = () => {
   return (
     <div className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex items-center gap-1 rounded-xl border-[1.5px] p-4 text-2xl text-white">
-      <Home className="m-2 shrink-0" />
-      <Link href="/" className="font-normal">
+      <Link href="/" passHref className="flex items-center gap-1 font-normal">
+        <Home className="m-2 shrink-0" />
         India
       </Link>
       <ChevronIcon className="shrink-0" />
@@ -14,11 +14,12 @@ const BreadCrumbs = () => {
       </Link>
       <ChevronIcon className="shrink-0" />
       {/* selected */}
-      <Link href="/maharashtra" className="text-secondary-500 font-semibold">
+      <Link
+        href="/maharashtra/nagpur"
+        className="text-secondary-500 font-semibold"
+      >
         Nagpur
       </Link>
-      {/* <ChevronIcon className="shrink-0" />
-      <span className="">Nagpur</span> */}
     </div>
   );
 };
