535ae18b397022c6ac1c3a8f5a174a41543f0905
Yaseer <yaseer@ctruh.com>
2025-07-25 18:54:50 +0530

    `Update BreadCrumbs component styles`

--- src/components/ui/BreadCrumbs/index.tsx
+++ src/components/ui/BreadCrumbs/index.tsx
@@ -15,7 +15,7 @@ const BreadCrumbs = ({ items }: BreadCrumbsProps) => {
   if (!items || items.length === 0) return null;
 
   return (
-    <div className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex items-center gap-1 rounded-xl border-[1.5px] p-4 text-2xl text-white">
+    <div className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex items-center gap-0.5 rounded-xl border-[1.5px] p-4 text-white">
       {items.map((item, index) => {
         const isLast = index === items.length - 1;
         const isFirst = index === 0;
@@ -26,8 +26,8 @@ const BreadCrumbs = ({ items }: BreadCrumbsProps) => {
           <Fragment key={index}>
             <Link
               href={item.href}
-              className={`flex items-center gap-1 ${
-                isLast
+              className={`flex items-center gap-1 text-2xl select-none ${
+                !isFirst && isLast
                   ? 'text-secondary-500 pointer-events-none font-semibold'
                   : 'font-normal'
               }`}
