dda526152aa301c33e02fd21ba52dfb20fa2c2ff
Yaseer <yaseer@ctruh.com>
2025-07-25 16:43:10 +0530

    `Refactor BreadCrumbs component: update span classes and styles`

--- src/components/ui/BreadCrumbs/index.tsx
+++ src/components/ui/BreadCrumbs/index.tsx
@@ -5,9 +5,10 @@ const BreadCrumbs = () => {
   return (
     <div className="bg-background-dark-300 font-dm-sans border-background-dark-100 mt-10 flex items-center gap-1 rounded-xl border-[1.5px] p-4 text-2xl text-white">
       <Home className="m-2 shrink-0" />
-      <span className="">India</span>
+      <span className="font-normal">India</span>
       <ChevronIcon className="shrink-0" />
-      <span className="">Maharashtra</span>
+      {/* selected */}
+      <span className="text-secondary-500 font-semibold">Maharashtra</span>
       {/* <ChevronIcon className="shrink-0" />
       <span className="">Nagpur</span> */}
     </div>
