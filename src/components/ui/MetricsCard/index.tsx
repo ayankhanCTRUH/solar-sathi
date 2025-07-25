3186302a2f0edb73a628cf930c89e1fd0a01f00a
Yaseer <yaseer@ctruh.com>
2025-07-25 12:38:02 +0530

    `Refactor MetricsCard component: update styles and layout`

--- src/components/ui/MetricsCard/index.tsx
+++ src/components/ui/MetricsCard/index.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 
 const MetricsCard = () => {
   return (
-    <div className="bg-background-dark-200 w-full border border-[red] text-white backdrop:blur-sm">
-      <h3>Annual Savings Generated</h3>
-      <div className="w-fit bg-[blue]">
-        <RupeeIcon />
+    <div className="bg-background-dark-200 w-full rounded-xl border border-neutral-400 px-5 py-4 text-white shadow-[0_8px_32px_0_rgba(0,0,0,0.20)] backdrop:blur-sm">
+      <div className="flex items-start justify-between">
+        <h3 className="font-dm-sans text-[24px] font-medium">
+          Annual Savings Generated
+        </h3>
+        <div className="bg-background-dark-300 w-fit rounded-xl p-[14px]">
+          <RupeeIcon className="h-9 w-9" />
+        </div>
       </div>
     </div>
   );
