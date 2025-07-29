4d7cb34622500286627ad56afc65adffd14cd7ff
ayankhanCTRUH <ayan@ctruh.com>
2025-07-29 20:43:24 +0530

    fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 'use client';
-import MapSection from '@/components/layout/MapSection';
+import dynamic from 'next/dynamic';
+
+const MapSection = dynamic(
+  () => import('@/components/layout/MapSection')
+)
 
 export default function Home() {
   return <MapSection />;
