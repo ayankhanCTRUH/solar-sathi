ad524a5717360ad0530e2c63242a820bc910d21c
Gulzeesh <gulzeesh@ctruh.com>
2025-08-19 19:35:22 +0530

    update: font family in ribbon

--- src/components/layout/Ribbon/index.tsx
+++ src/components/layout/Ribbon/index.tsx
@@ -15,7 +15,7 @@ const Ribbon = () => {
   }, []);
 
   return (
-    <div className="border-background-dark-200 bg-primary-500 leading-trim absolute inset-x-0 bottom-0 z-[1000] flex h-[74px] items-center justify-center border py-4 text-[32px] font-semibold text-white shadow-(--ribbon-shadow)">
+    <div className="border-background-dark-200 bg-primary-500 leading-trim font-dm-sans absolute inset-x-0 bottom-0 z-[1000] flex h-[74px] items-center justify-center border py-4 text-[32px] font-semibold text-white shadow-(--ribbon-shadow)">
       {RIBBON_DATA[index]}
     </div>
   );
