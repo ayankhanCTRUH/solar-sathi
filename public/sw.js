dbd4ee7864aaedd81e9a88972200e9de4f728748
Gulzeesh <gulzeesh@ctruh.com>
2025-08-20 11:55:44 +0530

    fix: pwa install button availability issue on browser

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// Service Worker for PWA
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', () => {});
